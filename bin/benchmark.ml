4826df3938acc6d44f4895261af5e9e5a6f6a1d4
Brice Decaestecker <brice.decaestecker@gmx.fr>
2025-09-12 06:28:08 +0200

    Improve feedback loop, stopping benchmarking naive

--- bin/benchmark.ml
+++ bin/benchmark.ml
@@ -6,29 +6,44 @@ let _timed_execution f =
   result, delta *. 1000.
 ;;
 
+let store_count = 10_000
+let get_count = 1000
+let query_count = 1000
+
 let bench (module I : Index.S with type config = Index.config_open_file) =
   let rand = Testability.reproducible_random () in
-  let functions = Testability.pseudo_random_functions rand 10_000 10 10 in
+  let functions = Testability.pseudo_random_functions rand store_count 10 10 in
   let index = I.init { file = "bench.idx"; mode = Create } in
   let _, store_time = _timed_execution @@ fun () -> I.store index functions in
   let _, get_time =
     _timed_execution
     @@ fun () ->
-    Testability.pick_n rand 1000 functions
+    Testability.pick_n rand get_count functions
     |> List.map Signature.CFunction.signature
     |> List.map (I.get index)
   in
-  let _, query_time =
+  let results, query_time =
     _timed_execution
     @@ fun () ->
-    List.init 1000 (fun _ -> Testability.pseudo_random_signature rand 10 10)
+    List.init query_count (fun _ -> Testability.pseudo_random_signature rand 10 10)
     |> List.map Index.Query.condense_signature
     |> List.map (I.query index)
+  in
+  let average_percentage_matching_query =
+    results
+    |> List.map (Fun.compose float_of_int List.length)
+    (* Matched / In store*)
+    |> List.map (fun matched -> 100.0 *. matched /. float_of_int store_count)
+    (* Sum *)
+    |> List.fold_left ( +. ) 0.0
+    (* Mean over total of query *)
+    |> fun total -> total /. float_of_int (List.length results)
   in
   [| I.id
    ; string_of_float store_time
    ; string_of_float get_time
    ; string_of_float query_time
+   ; string_of_float average_percentage_matching_query
   |]
 ;;
 
@@ -65,11 +80,11 @@ let pad_left width str =
 ;;
 
 let () =
-  let headers = [| "Index Type \\ Op(ms)"; "Store"; "Get"; "Query" |] in
+  let headers = [| "Index Type \\ Op(ms)"; "Store"; "Get"; "Query"; "Match %" |] in
   let headers_suffix = Array.make (Array.length headers) "---" in
   let results = Dynarray.of_array [| headers; headers_suffix |] in
   Dynarray.add_last results @@ bench (module InMemory);
-  Dynarray.add_last results @@ bench (module Index.FileBased);
+  (* Dynarray.add_last results @@ bench (module Index.FileBased); *)
   Dynarray.add_last results @@ bench (module Index.FileBasedSorted);
   let cell_widths =
     Array.mapi
